0aa0994ca6e9503ac34ab653ecf700cdf6990f32
ferry septian s <ferryseptiansusanto@gmail.com>
2026-07-11 12:54:15 +0700

    First Initialize

---
+++ Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define SYS_WKUP_Pin GPIO_PIN_0
+#define SYS_WKUP_GPIO_Port GPIOA
+#define PH_Pin GPIO_PIN_1
+#define PH_GPIO_Port GPIOA
+#define TDS_Pin GPIO_PIN_0
+#define TDS_GPIO_Port GPIOB
+#define TEMP_Pin GPIO_PIN_1
+#define TEMP_GPIO_Port GPIOB
+#define PUPUK_5_Pin GPIO_PIN_12
+#define PUPUK_5_GPIO_Port GPIOB
+#define PUPUK_4_Pin GPIO_PIN_13
+#define PUPUK_4_GPIO_Port GPIOB
+#define PUPUK_3_Pin GPIO_PIN_14
+#define PUPUK_3_GPIO_Port GPIOB
+#define PUPUK_2_Pin GPIO_PIN_15
+#define PUPUK_2_GPIO_Port GPIOB
+#define FLOWMETER_IN_Pin GPIO_PIN_8
+#define FLOWMETER_IN_GPIO_Port GPIOA
+#define PUPUK_1_Pin GPIO_PIN_15
+#define PUPUK_1_GPIO_Port GPIOA
+#define FLOWMETER_OUT_Pin GPIO_PIN_4
+#define FLOWMETER_OUT_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
